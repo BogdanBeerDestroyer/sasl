d10dc735edb05ddddf7d7ec0effd33681ec5241f
Sam Whited <sam@samwhited.com>
2022-08-03 09:33:25 -0400

    sasl: fix some file headers
    I'm not sure how these were not caught before, there is likely some bug
    in checkdoc that causes it to skip some files.
    
    Signed-off-by: Sam Whited <sam@samwhited.com>

--- example_plain_test.go
+++ example_plain_test.go
@@ -1,6 +1,6 @@
-// Copyright 2016 Sam Whited.
-// Use of this source code is governed by the BSD 2-clause license that can be
-// found in the LICENSE file.
+// Copyright 2016 The Mellium Contributors.
+// Use of this source code is governed by the BSD 2-clause
+// license that can be found in the LICENSE file.
 
 package sasl_test
 
